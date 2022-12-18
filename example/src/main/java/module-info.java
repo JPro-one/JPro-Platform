94c9a94bb04b28d59e7e357525726095c2fd1dc8
Besmir Beqiri <besmir.beqiri@logicbind.io>
2022-12-18 19:12:04 +0100

    (Re)add module descriptor for examples

---
+++ example/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module one.jpro.utils.example {
+    requires javafx.controls;
+    requires javafx.media;
+    requires jpro.webapi;
+    requires jpro.utils.htmlscrollpane;
+    requires one.jpro.media;
+    requires one.jpro.sound;
+    requires one.jpro.sessionmanager;
+
+    exports one.jpro.utils.example;
+}
