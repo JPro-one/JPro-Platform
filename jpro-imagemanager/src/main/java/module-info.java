3cfffb0a48029313899c290746478e9975648307
Besmir Beqiri <bb@sandec.de>
2023-09-12 10:17:43 +0200

    Remove unused module from `one.jpro.platform.imagemanager` module descriptor

--- jpro-imagemanager/src/main/java/module-info.java
+++ jpro-imagemanager/src/main/java/module-info.java
@@ -4,7 +4,6 @@
 module one.jpro.platform.imagemanager {
     requires java.desktop;
     requires javafx.graphics;
-    requires javafx.controls;
     requires org.slf4j;
     requires jpro.webapi;
     requires org.json;
