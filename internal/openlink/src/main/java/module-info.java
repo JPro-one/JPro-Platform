a82c28a20bc617d78cf0a87586d86ef0e813eb66
Besmir Beqiri <bb@sandec.de>
2023-09-27 19:53:34 +0200

    Improve OpenLink API by adding `null` checks and logging

--- internal/openlink/src/main/java/module-info.java
+++ internal/openlink/src/main/java/module-info.java
@@ -6,6 +6,7 @@
 module one.jpro.platform.internal.openlink {
     requires javafx.controls;
     requires org.jetbrains.annotations;
+    requires org.slf4j;
 
     exports one.jpro.platform.internal.openlink;
     exports one.jpro.platform.internal.openlink.util;
