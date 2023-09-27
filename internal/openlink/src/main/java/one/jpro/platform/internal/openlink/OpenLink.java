6cedffa91647dbea3399f5df568497bd544aafce
Besmir Beqiri <bb@sandec.de>
2023-09-27 19:53:34 +0200

    Improve OpenLink API by adding `null` checks and logging

--- internal/openlink/src/main/java/one/jpro/platform/internal/openlink/OpenLink.java
+++ internal/openlink/src/main/java/one/jpro/platform/internal/openlink/OpenLink.java
@@ -2,9 +2,12 @@ package one.jpro.platform.internal.openlink;
 
 import one.jpro.platform.internal.openlink.util.PlatformUtils;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Launches the default browser of the platform as a separate application process. The browser
@@ -14,12 +17,15 @@ import java.net.URL;
  */
 public interface OpenLink {
 
+    Logger logger = org.slf4j.LoggerFactory.getLogger(OpenLink.class);
+
     /**
      * Opens the given URL in the browser.
      *
      * @param url the URL to open
      */
     static void openURL(@NotNull URL url) {
+        Objects.requireNonNull(url, "URL cannot be null.");
         openURL(url.toString());
     }
 
@@ -29,16 +35,20 @@ public interface OpenLink {
      * @param url the URL string to open
      */
     static void openURL(@NotNull String url) {
-        try {
-            if (PlatformUtils.isMac()) {
-                Runtime.getRuntime().exec("open " + url);
-            } else if (PlatformUtils.isWindows()) {
-                Runtime.getRuntime().exec("start \"" + url + "\"");
-            } else if (PlatformUtils.isLinux()) {
-                Runtime.getRuntime().exec("xdg-open " + url);
+        Objects.requireNonNull(url, "URL cannot be null.");
+
+        if (PlatformUtils.isDesktop()) {
+            try {
+                List<String> command =
+                        PlatformUtils.isMac() ? List.of("open", url) :
+                                PlatformUtils.isWindows() ? List.of("start", url) : List.of("xdg-open", url);
+                logger.debug("Opening URL with command {}", command);
+                Runtime.getRuntime().exec(command.toArray(String[]::new));
+            } catch (IOException ex) {
+                throw new UnsupportedOperationException("Unable to open the browser.", ex);
             }
-        } catch (IOException ex) {
-            throw new UnsupportedOperationException("Unable to open the browser!", ex);
+        } else {
+            throw new UnsupportedOperationException("Platform not supported.");
         }
     }
 }
