4170df7303b74ec5472137732ea592b771202138
floriankirmaier <florian.kirmaier@gmail.com>
2023-12-04 16:04:52 +0100

    Added youtube node

---
+++ jpro-youtube/example/src/main/java/one/jpro/platform/youtube/example/YoutubeExample.java
@@ -0,0 +1,24 @@
+package one.jpro.platform.youtube.example;
+
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import one.jpro.platform.youtube.YoutubeNode;
+import org.scenicview.ScenicView;
+
+public class YoutubeExample extends Application {
+
+    @Override
+    public void start(javafx.stage.Stage stage) throws Exception {
+        var pin = new VBox();
+
+        pin.getChildren().add(new StackPane(new YoutubeNode("dQw4w9WgXcQ")));
+        pin.getChildren().add(new StackPane(new YoutubeNode("oqAsIcoN9MY")));
+        stage.setScene(new javafx.scene.Scene(new VBox(pin)));
+
+        ScenicView.show(stage.getScene());
+
+        stage.show();
+    }
+}
