ef58e99588afeb0e83c82677753332d246f01bf7
floriankirmaier <florian.kirmaier@gmail.com>
2024-03-04 13:49:30 +0100

    Added the user-api to the basic login app

--- jpro-auth/routing/src/main/java/one/jpro/platform/auth/routing/UserAPI.java
+++ jpro-auth/routing/src/main/java/one/jpro/platform/auth/routing/UserAPI.java
@@ -4,14 +4,30 @@ import javafx.collections.ObservableMap;
 import one.jpro.platform.auth.core.authentication.User;
 import org.json.JSONObject;
 
+/**
+ * The UserAPI class is a simple
+ * class that provides a way to
+ * get and set the user object
+ * from the session.
+ * @author floriankirmaier
+ */
 public class UserAPI {
 
-    ObservableMap<String, String> session;
+    private ObservableMap<String, String> session;
 
+    /**
+     * The constructor for the UserAPI class
+     * @param session
+     */
     public UserAPI(ObservableMap<String, String> session) {
         this.session = session;
     }
 
+    /**
+     * The getUser method returns the user
+     * object from the session
+     * @return User
+     */
     public final User getUser() {
         final var userJsonString = session.get("user");
         if (userJsonString != null) {
@@ -22,9 +38,14 @@ public class UserAPI {
         }
     }
 
-    public final void setUser(User value) {
-        if (value != null) {
-            session.put("user", value.toJSON().toString());
+    /**
+     * The setUser method sets the user
+     * object in the session
+     * @param user
+     */
+    public final void setUser(User user) {
+        if (user != null) {
+            session.put("user", user.toJSON().toString());
         } else {
             session.remove("user");
         }
