0c47541ad37ed109cea96d139d2eb3e0026e6f31
Besmir Beqiri <bb@sandec.de>
2024-03-08 13:21:02 +0100

    Define all native dependencies for the currently supported `jpro-media` module examples

--- jpro-media/example/src/main/java/module-info.java
+++ jpro-media/example/src/main/java/module-info.java
@@ -13,18 +13,18 @@ module one.jpro.platform.media.example {
     requires one.jpro.platform.media;
     requires one.jpro.platform.file;
 
-//    requires org.bytedeco.ffmpeg.windows.x86_64;
-//    requires org.bytedeco.opencv.windows.x86_64;
-//    requires org.bytedeco.openblas.windows.x86_64;
-//    requires org.bytedeco.videoinput.windows.x86_64;
+    requires org.bytedeco.ffmpeg.windows.x86_64;
+    requires org.bytedeco.opencv.windows.x86_64;
+    requires org.bytedeco.openblas.windows.x86_64;
+    requires org.bytedeco.videoinput.windows.x86_64;
 
-//    requires org.bytedeco.ffmpeg.linux.x86_64;
-//    requires org.bytedeco.opencv.linux.x86_64;
-//    requires org.bytedeco.openblas.linux.x86_64;
+    requires org.bytedeco.ffmpeg.linux.x86_64;
+    requires org.bytedeco.opencv.linux.x86_64;
+    requires org.bytedeco.openblas.linux.x86_64;
 
-//    requires org.bytedeco.ffmpeg.macosx.x86_64;
-//    requires org.bytedeco.opencv.macosx.x86_64;
-//    requires org.bytedeco.openblas.macosx.x86_64;
+    requires org.bytedeco.ffmpeg.macosx.x86_64;
+    requires org.bytedeco.opencv.macosx.x86_64;
+    requires org.bytedeco.openblas.macosx.x86_64;
 
     requires org.bytedeco.ffmpeg.macosx.arm64;
     requires org.bytedeco.opencv.macosx.arm64;
