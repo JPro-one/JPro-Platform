cf29e308420b555b91f59797a41c01b8e3578e55
Florian Kirmaier <florian.kirmaier@gmail.com>
2025-01-15 15:07:34 +0100

    Added simple filter, for error page and not found page. (#57)

--- jpro-routing/core/src/main/scala/one/jpro/platform/routing/Filters.scala
+++ jpro-routing/core/src/main/scala/one/jpro/platform/routing/Filters.scala
@@ -61,4 +61,26 @@ object Filters {
     route.and(function)
   }
 
+  def errorPage(): Filter = errorPage((request, ex) => Response.node(new Label("Error: " + ex.getMessage)))
+  def errorPage(biFunction: BiFunction[Request, Throwable, Response]): Filter = {
+    route => { request =>
+      try {
+        val r = route.apply(request)
+        Response.fromFuture(r.future.map(x => Response.fromResult(x)).exceptionally { ex =>
+          biFunction.apply(request, ex)
+        })
+      } catch {
+        case ex: Throwable =>
+          biFunction.apply(request, ex)
+      }
+    }
+  }
+
+  def notFoundPage(): Filter = {
+    notFoundPage((request) => Response.node(new Label("Not Found: " + request.getPath())))
+  }
+  def notFoundPage(function: Route): Filter = { route =>
+    route.and(function)
+  }
+
 }
