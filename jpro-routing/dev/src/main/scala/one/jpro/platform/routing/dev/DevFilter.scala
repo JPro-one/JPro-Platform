5d62edf0edbc4318b241c1219807dc3c18b88fbe
floriankirmaier <florian.kirmaier@gmail.com>
2025-03-08 18:21:17 +0100

    Enhance DevFilter - focused element no longer grows indefinite.

--- jpro-routing/dev/src/main/scala/one/jpro/platform/routing/dev/DevFilter.scala
+++ jpro-routing/dev/src/main/scala/one/jpro/platform/routing/dev/DevFilter.scala
@@ -95,8 +95,12 @@ object DevFilter {
           } else None
           text <-- ("LayoutCounter: " + layouts.map(_.toString).getOrElse("-"))
         }
-        this <++ new Label {
+        this <++ new Label { LABEL =>
+          getStyleClass.add("devfilter-focus-label")
           text <-- ("Focused: " + (if(scene == null) "" else scene.focusOwner))
+          tooltip = new Tooltip() {
+            text <-- ("Focused: " + (if(LABEL.scene == null) "" else LABEL.scene.focusOwner))
+          }
         }
         //this <++ new Label() {
         //  text <-- (if(request == null) "-" else "request: " + request)
