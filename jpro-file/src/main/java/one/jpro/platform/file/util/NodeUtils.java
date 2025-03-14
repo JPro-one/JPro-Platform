36862fa151d0170d2d4fb13ad1e5d30931d57888
Besmir Beqiri <bb@sandec.de>
2025-03-14 04:28:31 +0100

    `NodeUtils.addEventHandler` now replaces the older handler with the new one

--- jpro-file/src/main/java/one/jpro/platform/file/util/NodeUtils.java
+++ jpro-file/src/main/java/one/jpro/platform/file/util/NodeUtils.java
@@ -29,6 +29,7 @@ public interface NodeUtils {
      * @return the value of the property associated with the given key,
      *         or the default value if the property does not exist
      */
+    @SuppressWarnings("unchecked")
     static <K, V> V getPropertyValue(Node node, K key, V defaultValue) {
         final Map<Object, Object> nodeProperties = node.getProperties();
 
@@ -44,14 +45,17 @@ public interface NodeUtils {
     }
 
     /**
-     * Adds an event handler to the specified node for the given event type.
-     * If an event handler for the event type is already added, it will not add another one.
+     * Adds (or replaces) an event handler on the specified node for the given event type.
+     * If an event handler for this event type was previously added via this method, it is
+     * first removed before adding the new handler. Only one handler per event type is
+     * maintained through this utility.
      *
      * @param node         the node to which the event handler will be added
-     * @param eventType    the type of the event to handle
+     * @param eventType    the event type to handle
      * @param eventHandler the event handler to be added
-     * @param <T>          the type of the event
+     * @param <T>          the specific subclass of {@link Event} being handled
      */
+    @SuppressWarnings("unchecked")
     static <T extends Event> void addEventHandler(Node node, EventType<T> eventType,
                                                   EventHandler<? super T> eventHandler) {
         final Map<Object, Object> nodeProperties = node.getProperties();
@@ -59,10 +63,14 @@ public interface NodeUtils {
         // Create the event handler key for the given event type
         final String eventTypeKey = EVENT_HANDLER_KEY + "_" + eventType.getName();
 
-        // Check if the event handler is already added
-        if (!nodeProperties.containsKey(eventTypeKey)) {
-            node.addEventHandler(eventType, eventHandler);
-            nodeProperties.put(eventTypeKey, eventHandler);
+        // Remove old event handler if present
+        if (nodeProperties.containsKey(eventTypeKey)) {
+            EventHandler<? super T> oldEventHandler = (EventHandler<? super T>) nodeProperties.get(eventTypeKey);
+            node.removeEventHandler(eventType, oldEventHandler);
         }
+
+        // Add and store the new event handler
+        node.addEventHandler(eventType, eventHandler);
+        nodeProperties.put(eventTypeKey, eventHandler);
     }
 }
