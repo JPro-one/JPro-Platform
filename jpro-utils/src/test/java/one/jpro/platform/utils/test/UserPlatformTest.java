4ec7e211b3460cd03af21ade81518f4b69aa913c
floriankirmaier <florian.kirmaier@gmail.com>
2025-04-04 21:46:43 +0200

    small improvements based on code review

--- jpro-utils/src/test/java/one/jpro/platform/utils/test/UserPlatformTest.java
+++ jpro-utils/src/test/java/one/jpro/platform/utils/test/UserPlatformTest.java
@@ -2,8 +2,9 @@ package one.jpro.platform.utils.test;
 
 import javafx.scene.input.KeyCode;
 import one.jpro.platform.utils.UserPlatform;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserPlatformTest {
 
@@ -17,53 +18,53 @@ public class UserPlatformTest {
     @Test
     public void testMac1() {
         var platform = UserPlatform.simulateWeb("macOS", "MacIntel");
-        Assertions.assertTrue(platform.isMac());
-        Assertions.assertFalse(platform.isWindows());
-        Assertions.assertFalse(platform.isLinux());
-        Assertions.assertEquals(platform.getModifierKey(), KeyCode.META);
+        assertTrue(platform.isMac());
+        assertFalse(platform.isWindows());
+        assertFalse(platform.isLinux());
+        assertEquals(platform.getModifierKey(), KeyCode.META);
     }
 
     @Test
     public void testMac2() {
         var platform = UserPlatform.simulateWeb(null, "MacIntel");
-        Assertions.assertTrue(platform.isMac());
-        Assertions.assertFalse(platform.isWindows());
-        Assertions.assertFalse(platform.isLinux());
-        Assertions.assertEquals(platform.getModifierKey(), KeyCode.META);
+        assertTrue(platform.isMac());
+        assertFalse(platform.isWindows());
+        assertFalse(platform.isLinux());
+        assertEquals(platform.getModifierKey(), KeyCode.META);
     }
 
     @Test
     public void testMac3() {
         var platform = UserPlatform.simulateNative("Mac OS X");
-        Assertions.assertTrue(platform.isMac());
-        Assertions.assertFalse(platform.isWindows());
-        Assertions.assertFalse(platform.isLinux());
-        Assertions.assertFalse(platform.isMobile());
-        Assertions.assertEquals(platform.getModifierKey(), KeyCode.META);
+        assertTrue(platform.isMac());
+        assertFalse(platform.isWindows());
+        assertFalse(platform.isLinux());
+        assertFalse(platform.isMobile());
+        assertEquals(platform.getModifierKey(), KeyCode.META);
     }
 
     @Test
     public void testWin1() {
         var platform = UserPlatform.simulateWeb("Windows", "Win32");
-        Assertions.assertFalse(platform.isMac());
-        Assertions.assertTrue(platform.isWindows());
-        Assertions.assertFalse(platform.isLinux());
-        Assertions.assertEquals(platform.getModifierKey(), KeyCode.CONTROL);
+        assertFalse(platform.isMac());
+        assertTrue(platform.isWindows());
+        assertFalse(platform.isLinux());
+        assertEquals(platform.getModifierKey(), KeyCode.CONTROL);
     }
     @Test
     public void testWin2() {
         var platform = UserPlatform.simulateWeb(null, "Win32");
-        Assertions.assertFalse(platform.isMac());
-        Assertions.assertTrue(platform.isWindows());
-        Assertions.assertFalse(platform.isLinux());
-        Assertions.assertEquals(platform.getModifierKey(), KeyCode.CONTROL);
+        assertFalse(platform.isMac());
+        assertTrue(platform.isWindows());
+        assertFalse(platform.isLinux());
+        assertEquals(platform.getModifierKey(), KeyCode.CONTROL);
     }
 
     @Test
     public void testIphone() {
         var platform = UserPlatform.simulateWeb(null, "iPhone");
-        Assertions.assertFalse(platform.isMac());
-        Assertions.assertFalse(platform.isWindows());
-        Assertions.assertFalse(platform.isLinux());
+        assertFalse(platform.isMac());
+        assertFalse(platform.isWindows());
+        assertFalse(platform.isLinux());
     }
 }
