60b28bc1d64b795e41ddbe5b1af27983e0d865f3
floriankirmaier <florian.kirmaier@gmail.com>
2025-08-22 16:54:48 +0200

    Updated javadoc, and added new method AuthUIProviders.combine which allows to define the container.

--- jpro-auth/routing/src/main/java/one/jpro/platform/auth/routing/AuthUIProvider.java
+++ jpro-auth/routing/src/main/java/one/jpro/platform/auth/routing/AuthUIProvider.java
@@ -3,6 +3,12 @@ package one.jpro.platform.auth.routing;
 import javafx.scene.Node;
 import one.jpro.platform.routing.Filter;
 
+/**
+ * This class represents an Authentication Method, represented by:
+ * 1. A Node that can be used to display the authentication UI.
+ * 2. A Filter that can be used to integrate the authentication method
+ *   into theo RouteApp.
+ */
 public interface AuthUIProvider {
     Node createAuthenticationNode();
     Filter createFilter();
--- jpro-auth/routing/src/main/java/one/jpro/platform/auth/routing/AuthUIProviders.java
+++ jpro-auth/routing/src/main/java/one/jpro/platform/auth/routing/AuthUIProviders.java
@@ -15,16 +15,33 @@ import one.jpro.platform.routing.Filter;
 import one.jpro.platform.routing.LinkUtil;
 import one.jpro.platform.routing.Response;
 import org.jetbrains.annotations.NotNull;
+import javafx.scene.layout.Pane;
 
 import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class AuthUIProviders {
 
+    /**
+     * Creates an AuthUIProvider for OAuth2/OpenID authentication.
+     *
+     * @param openidAuthProvider the OpenID authentication provider
+     * @param userSession        the user session to be used
+     * @return an AuthUIProvider that provides a button for OAuth2 authentication
+     */
     public static AuthUIProvider createOAuth2(@NotNull OpenIDAuthenticationProvider openidAuthProvider,
                                                       @NotNull UserSession userSession) {
         return createOAuth2(openidAuthProvider, userSession, () -> new Button("Login"));
     }
+
+    /**
+     * Creates an AuthUIProvider for OAuth2/OpenID authentication with a custom button.
+     *
+     * @param openidAuthProvider the OpenID authentication provider
+     * @param userSession        the user session to be used
+     * @param createButton       a supplier that provides the button to be used for authentication
+     * @return an AuthUIProvider that provides a button for OAuth2 authentication
+     */
     public static AuthUIProvider createOAuth2(@NotNull OpenIDAuthenticationProvider openidAuthProvider,
                                                       @NotNull UserSession userSession,
                                                       @NotNull Supplier<Button> createButton) {
@@ -44,11 +61,19 @@ public class AuthUIProviders {
             }
         };
     }
+
+    /**
+     * Creates an AuthUIProvider for Google OAuth2/OpenID authentication.
+     * This AuthUIProvider uses a specific button for Google login.
+     */
     public static AuthUIProvider createGoogle(@NotNull OpenIDAuthenticationProvider openidAuthProvider,
                                               @NotNull UserSession userSession) {
         return createOAuth2(openidAuthProvider, userSession, () -> new GoogleLoginButton());
     }
 
+    /**
+     * Creates an AuthUIProvider for basic authentication using a username and password.
+     */
     public static AuthUIProvider createBasicProvider(@NotNull BasicAuthenticationProvider authProvider,
                                                                     @NotNull UserSession userSession) {
         return new AuthUIProvider() {
@@ -92,22 +117,32 @@ public class AuthUIProviders {
         };
     }
 
+    /**
+     * Combines multiple AuthUIProviders into a single AuthUIProvider.
+     */
     public static AuthUIProvider combine(AuthUIProvider... providers) {
+        return combine(() -> new VBox(), providers);
+    }
+
+    /**
+     *
+     */
+    public static AuthUIProvider combine(Supplier<Pane> paneSupplier, AuthUIProvider... providers) {
         return new AuthUIProvider() {
             @Override
             public Node createAuthenticationNode() {
-                return new VBox() {{
-                    for (var provider : providers) {
-                        getChildren().add(provider.createAuthenticationNode());
-                    }
-                }};
+                var pane = paneSupplier.get();
+                for (var provider : providers) {
+                    pane.getChildren().add(provider.createAuthenticationNode());
+                }
+                return pane;
             }
 
             @Override
             public Filter createFilter() {
                 return Arrays.stream(providers)
-                        .map(a -> a.createFilter())
-                        .reduce((a,b) -> a.compose(b)).get();
+                        .map(AuthUIProvider::createFilter)
+                        .reduce(Filter::compose).orElse(Filter.empty());
             }
         };
     }
