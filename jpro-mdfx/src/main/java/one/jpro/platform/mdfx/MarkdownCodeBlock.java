c80557998dd09a22b4ca5ac04607cd3ca29591d0
Daniel Gyoerffy <49782833+Dansoftowner@users.noreply.github.com>
2026-04-21 18:00:56 +0200

    Add "rest" as alias for "http" in markdown code block

--- jpro-mdfx/src/main/java/one/jpro/platform/mdfx/MarkdownCodeBlock.java
+++ jpro-mdfx/src/main/java/one/jpro/platform/mdfx/MarkdownCodeBlock.java
@@ -65,6 +65,7 @@ public class MarkdownCodeBlock extends StackPane {
         LANGUAGE_TO_GRAMMAR.put("kotlin", "grammars/java.tmLanguage.json");
         LANGUAGE_TO_GRAMMAR.put("kt", "grammars/java.tmLanguage.json");
         LANGUAGE_TO_GRAMMAR.put("http", "grammars/http.tmLanguage.json");
+        LANGUAGE_TO_GRAMMAR.put("rest", "grammars/http.tmLanguage.json");
     }
 
     // CSS-styleable property
